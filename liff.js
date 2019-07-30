0b9d7f6d26781fe0b8b1fc19b8dac37d3d29bb25
ExploreLab <34715165+ExploreLab@users.noreply.github.com>
2019-07-30 17:15:28 +0700

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID = '5c251574-e71a-4326-b455-ad7c5c64b9e4'; // LED, Button
+const USER_SERVICE_UUID = 'b769504c-cc14-42fd-a6be-7fd550cbb8c1'; // LED, Button 5c251574-e71a-4326-b455-ad7c5c64b9e4
 // User service characteristics
 const LED_CHARACTERISTIC_UUID = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
